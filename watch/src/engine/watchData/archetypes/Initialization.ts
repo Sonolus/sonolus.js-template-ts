4499e24fa53378b5454c17a39fa129c495b3dbec
Burrito <burritoyangster@gmail.com>
2026-02-08 09:14:40 +0800

    Support Sonolus 1.1.0 (#22)

--- watch/src/engine/watchData/archetypes/Initialization.ts
+++ watch/src/engine/watchData/archetypes/Initialization.ts
@@ -10,10 +10,21 @@ export class Initialization extends Archetype {
             background: true,
         })
 
-        ui.progress.set({
+        ui.progress.bar.set({
             anchor: screen.rect.lb.add(new Vec(0.05, 0.05)),
             pivot: { x: 0, y: 0 },
             size: { x: screen.rect.w - 0.1, y: 0.15 * ui.configuration.progress.scale },
+            rotation: 0,
+            alpha: ui.configuration.progress.alpha,
+            horizontalAlign: HorizontalAlign.Center,
+            background: true,
+        })
+        ui.progress.graph.set({
+            anchor: screen.rect.lb
+                .add(new Vec(0.05, 0.1))
+                .add(new Vec(0, 0.15 * ui.configuration.progress.scale)),
+            pivot: { x: 0, y: 0 },
+            size: { x: screen.rect.w - 0.1, y: 0.3 * ui.configuration.progress.scale },
             rotation: 0,
             alpha: ui.configuration.progress.alpha,
             horizontalAlign: HorizontalAlign.Center,
